22e4645d8e4e53508191f24b921a18f22104dd33
Abdeladhim Abbassi <abbassi.abdeladhim31791@gmail.com>
2021-06-10 13:15:09 +0200

    Update BankingApp.java

--- src/com/banking/BankingApp.java
+++ src/com/banking/BankingApp.java
@@ -1,16 +1,15 @@
 package com.banking;
 
-/**
- * In this project, you will be learning how to take input from the user using Scanner class,
- * basics about String, how to print in java, variables, if/else statements, methods, loops, etc.
- * In simple banking application you will be coding the simple bank operations like 
- * - Check balance
- * - Deposit
- * - Withdraw
- * - Exit 
- */
 public class BankingApp {
-
+	/**
+	 * In this project, you will be learning how to take input from the user using Scanner class,
+	 * basics about String, how to print in java, variables, if/else statements, methods, loops, etc.
+	 * In simple banking application you will be coding the simple bank operations like 
+	 * - Check balance
+	 * - Deposit
+	 * - Withdraw
+	 * - Exit 
+	 */
 	public static void main(String[] args) {
 		BankAccount myAccount = new BankAccount("Abdeladhim Abbassi", "0012345");
 		myAccount.showMenu();
